000210714c500fd71474b81fef73acf65e48c8f1
ibrahimamar07 <ibrahimamaralfanani07@gmail.com>
2026-06-19 07:34:41 +0700

    fix storage

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -23,7 +23,7 @@ class Note extends Model
     }
 
     public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
+{
+    return $this->belongsTo(User::class, 'user_id', 'idUser'); // ← tambahkan 'idUser'
+}
 }
